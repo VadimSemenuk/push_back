170d1346d35426cffdb3bd5f9423561ff57e8d2c
Vadim Semenyuk <vadim54787@gmail.com>
2025-07-18 22:33:20 +0300

    1

--- api/index.js
+++ api/index.js
@@ -25,11 +25,19 @@ webPush.setVapidDetails(
 //     }
 // };
 
+// const subscription = {
+//     endpoint: "https://fcm.googleapis.com/fcm/send/fmR74_ft4IE:APA91bFW4FKV68MA8_g-wpuRQq6uijCSvgCJvORWv679uN7WYEec_sc3imJj8TGR4qvlYSwu-Ldq1KEJ5drUfhXz8x7OwZ_WUHsrqCgikdocfQ-pOpMXc1M9IObILm1pXKMM0UpUfJzF",
+//     keys: {
+//         auth: "FbJbtTBXXqrUbuNUBl9mrQ",
+//         p256dh: "BMsgIHw0ungZXxAL81UlQPJQcUxoqoe4sVwOeozV_xzzrAbkFLeQ1_DAm1PCT51Shc0EjYDUqteRMx7jO0f_8dg"
+//     }
+// };
+
 const subscription = {
-    endpoint: "https://fcm.googleapis.com/fcm/send/fmR74_ft4IE:APA91bFW4FKV68MA8_g-wpuRQq6uijCSvgCJvORWv679uN7WYEec_sc3imJj8TGR4qvlYSwu-Ldq1KEJ5drUfhXz8x7OwZ_WUHsrqCgikdocfQ-pOpMXc1M9IObILm1pXKMM0UpUfJzF",
+    endpoint: "https://web.push.apple.com/QKVO5t5dmLQcy2EDslboEBOZB1g23wFBBN6VJScdRuIOmgl-LMFpG2lv_PjD97K-T0RQmU3hubkq00eH740cl2v9NxKocgwLVQCJddZgctgt8Uo_n8QxhRuGabmbA9gKDNZoXFBFGZ0otIWEBWtoefoiunkn-N_QdpzYy3GgQiY",
     keys: {
-        auth: "FbJbtTBXXqrUbuNUBl9mrQ",
-        p256dh: "BMsgIHw0ungZXxAL81UlQPJQcUxoqoe4sVwOeozV_xzzrAbkFLeQ1_DAm1PCT51Shc0EjYDUqteRMx7jO0f_8dg"
+        auth: "uFL7y9e6c39fwBy7liIBCw",
+        p256dh: "BBkNtG6ImOBjDEvdz9M2grXvGYAcW63nzFouirztpmQkh41ElaHVBCnUREF9FisVHC10JaGbNebtSXfGJxPEYWI"
     }
 };
 
